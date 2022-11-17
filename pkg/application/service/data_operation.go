5d9ee4bbc4550e1cbd0d8c9d792561d8bca2104b
neverett8fr <neverett8fr@btinternet.com>
2022-11-17 15:36:45 +0000

    remove 'test' function

--- pkg/application/service/data_operation.go
+++ pkg/application/service/data_operation.go
@@ -1,21 +1,13 @@
 package service
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
 	application "tempest-data-service/pkg/application/entities"
 
-	"cloud.google.com/go/storage"
 	"github.com/gorilla/mux"
-	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"
 )
 
 func newDataOperation(r *mux.Router) {
-	r.HandleFunc("/data/test/{username}", tt).Methods(http.MethodGet)
 	r.HandleFunc("/data/{username}/{item}", userFileContent).Methods(http.MethodGet)
 }
 
@@ -37,41 +29,4 @@ func userFileContent(w http.ResponseWriter, r *http.Request) {
 	body := application.NewResponse(string(fileContent))
 	writeReponse(w, r, body)
 
-}
-
-func tt(w http.ResponseWriter, r *http.Request) {
-
-	params := mux.Vars(r)
-	username := params["username"]
-
-	client, err := storage.NewClient(context.Background(), option.WithoutAuthentication())
-	if err != nil {
-		body := application.NewResponse(nil, fmt.Errorf("error"))
-		writeReponse(w, r, body)
-		return
-	}
-	bkt := client.Bucket("test-bucket-gfdjgfdhg")
-
-	var names []string
-	it := bkt.Objects(context.Background(), &storage.Query{
-		Prefix: username,
-	})
-	for {
-		attrs, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		names = append(names, attrs.Name)
-	}
-
-	rdr, _ := bkt.Object(names[2]).NewReader(context.Background())
-
-	byt, _ := io.ReadAll(rdr)
-
-	body := application.NewResponse(string(byt))
-
-	writeReponse(w, r, body)
 }
